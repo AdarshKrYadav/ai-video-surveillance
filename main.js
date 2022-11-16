6f5c77ee0a46326971651f6da0e268c7695ba418
AdarshKrYadav <96909789+AdarshKrYadav@users.noreply.github.com>
2022-11-16 17:56:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 video="";
-object_detector="";
 status="";
+object=[];
 function preload(){
     video=createVideo("video.mp4");
     video.hide();
@@ -12,10 +12,22 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
-
+    if(status!=""){
+        object_detector.detect(video,gotresult);
+        for(var i=0;i<object.length;i++){
+            document.getElementById("objectStatus").innerHTML="Status : Objects Detected";
+            document.getElementById("objectsDetected").innerHTML="Number of objects detected are : "+objects.length;
+            stroke("#0F9D58");
+            fill("#0F9D58");
+          var percent=floor(object[i].confidence*100); 
+          text(object[i].label+" "+percent+"%",object[i].x,object[i].y);
+          noFill();
+          rect(object[i].x, object[i].y , object[i].width, object[i].height); 
+        }
+    }
 }
 function startFunction(){
-    object_detector=ml5.objectDetector("cocossd",modelloaded);
+    objectDetector=ml5.objectDetector("cocossd",modelloaded);
     document.getElementById("objectStatus").innerHTML="Status : Detecting Object ";
 }
 function modelloaded(){
@@ -24,4 +36,13 @@ function modelloaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotresult(error,result){
+    if(error){
+        console.log(error);
+    }  
+    else{
+        console.log(result);
+        object=result;
+    }
 }
